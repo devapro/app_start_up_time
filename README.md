c637d7e265aa31e6b9e44eb03f00d4f269a73ea2
Arsenii Kharlanow <arsenyzp@gmail.com>
2023-03-19 15:57:19 +0300

    added new script

--- README.md
+++ README.md
@@ -23,13 +23,18 @@ buildscript {
 
 Script for running application N times and collect statistics of the time from start to the first frame
 It can be useful for comparing start-up time between releases.
-Example of the usage:
+
+### Benchmark
 
 ```bash
 /bin/bash ./startup_time_rooted.sh release/v1.0.1 release/v1.0.2 Phone_API_30
 ```
 
+Optional parameters:
+
 **Phone_API_30** - name of the emulator (or you can use real device) - device/emulator should be rooted
+
+Required parameters:
 **release/v1.0.1** - previous release brunch
 **release/v1.0.2** - new release branch
 
@@ -38,3 +43,8 @@ Or for non rooted device, you can use this script, the difference only in Lock c
 /bin/bash ./startup_time.sh release/v1.0.1 release/v1.0.2 Phone_API_30
 ```
 
+### Activity manager
+
+```bash
+/bin/bash ./startup_time_am.sh release/v1.0.1 release/v1.0.2 Phone_API_30
+```
---
+++ startup_time_am.sh
@@ -0,0 +1,120 @@
+#! /bin/bash
+
+PACKAGE_NAME="com.example.starttime.measure"
+FIRST_ACTIVITY="com.example.starttime.measure/.MainActivity"
+RELEASE_BUILD_VARIANT="assembleRelease"
+
+run_tests () {
+	echo "Run tests"
+
+	adb logcat -c
+	touch /tmp/logcat.txt
+  cp /dev/null /tmp/logcat.txt
+
+	TOTAL_TIME=0
+	TOTAL_CYCLES=3
+	MAX_TIME=0
+	MIN_TIME=0
+	for i in $(seq 1 $TOTAL_CYCLES)
+		do
+			adb shell am force-stop $PACKAGE_NAME >> /dev/null
+			adb shell pm clear $PACKAGE_NAME >> /dev/null
+			adb shell am start-activity -n ${FIRST_ACTIVITY} >> /dev/null
+			sleep 1
+			adb logcat -d -e Display >> /tmp/logcat.txt
+      adb logcat -c
+		done
+
+	cp /dev/null /tmp/logcat2.txt
+  cat /tmp/logcat.txt | grep ${FIRST_ACTIVITY} >> /tmp/logcat2.txt
+
+  TIMES=$(cat /tmp/logcat2.txt | grep -o '+[0-9]*s*[0-9]*ms' | cut -c 2- | sed 's/[^0-9]*//g' )
+  TOTAL_TIME=$(echo $TIMES | awk '{s+=$1}END{print s}' )
+  TOTAL_CYCLES=$(echo $TIMES | wc -l )
+
+  ARRAY=( $( echo $TIMES) )
+  for RESULT in "${ARRAY[@]}"
+  do
+     :
+     if [[ $MAX_TIME < $RESULT ]]; then
+        MAX_TIME=$RESULT
+     fi
+     if [[ $MIN_TIME == 0 ]]; then
+       MIN_TIME=$RESULT
+     fi
+     if [[ $MIN_TIME > $RESULT ]]; then
+       MIN_TIME=$RESULT
+     fi
+  done
+
+  echo "Average time: $(($TOTAL_TIME/$TOTAL_CYCLES))"
+  echo "Max time: $MAX_TIME"
+  echo "Min time: $MIN_TIME"
+}
+
+install () {
+	echo "Uninstall apk"
+	adb uninstall $PACKAGE_NAME >> /dev/null
+	if [ $? -eq 0 ]; then
+	   echo OK
+	else
+	   echo Was not installed
+	fi
+
+  echo "Install apk"
+  adb install -d "/tmp/$1.apk"
+  if [ $? -eq 0 ]; then
+  	   echo OK
+  	else
+  	   echo Was not installed
+  	   exit 1
+  	fi
+}
+
+build () {
+	./gradlew clean >> /dev/null
+	### install release build
+	echo "Build release apk"
+	./gradlew $RELEASE_BUILD_VARIANT >> /dev/null
+	if [ $? -eq 0 ]; then
+	   echo OK
+	   ### replace with relevant path
+	   cp ./app/build/outputs/apk/release/app-release.apk "/tmp/$1.apk"
+	else
+	   echo FAIL
+	   exit 1
+	fi
+}
+
+
+### run emulator
+if [ -z "$3" ]; then
+  echo "You didn't provide emulator name. Will be used active emulator or device"
+else
+  $ANDROID_HOME/emulator/emulator -avd $1  >> /dev/null &
+  adb wait-for-device
+  sleep 100
+fi
+
+#git checkout $2 #release/v1.0.1
+#
+#build 1
+#
+#git checkout $3 #release/v1.0.2
+#
+#build 2
+
+install 1
+
+run_tests
+
+install 2
+
+run_tests
+
+### turn of emulator
+if [ -z "$3" ]; then
+  echo "You didn't provide emulator name. Was used active emulator or device"
+else
+  adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
+fi
